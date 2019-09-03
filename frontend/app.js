0e79bb56a1989e2ab8c4edafc1d28052a55d2b43
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-03 13:01:20 +0200

    first commit

---
+++ frontend/app.js
@@ -0,0 +1,9 @@
+function botonAPI() {
+  fetch('http://localhost:3000')
+  .then(res => res.text())
+  .then(res => {
+    document.querySelector('.res').innerHTML = `Esto es lo que devuelve la peticion = ${res}
+    <h1>Bienvenidos</h1>`;
+  })
+  .catch(err => console.log(err))  
+}
