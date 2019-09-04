40e6b3c0524b13cfd1e821453b0ce10cb8909516
Francisco Sierra <franjrsierra18@gmail.com>
2019-09-04 12:46:54 +0200

    agregado un nav bar que nos ayuda a modificar ruta

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -20,5 +20,31 @@ const registro = event => {
       password: event.target.password.value,
     }),
   })
-  
+  .then (res => res.json())
+  .then (console.log)
+}
+const reg = `<div class="container">
+<form onsubmit="registro(event)">
+  <div class="form-group">
+    <label for="InputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="usuario">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>`;
+const Home = `<div>Home</div>`;
+const contenido = document.querySelector('.contenido');
+const router = {
+ '/':Home,
+ '/registro':reg
+}
+const onNavClick = ruta => {
+  history.pushState({},
+    ruta + ' | login simple',
+    location.origin + ruta),
+    contenido.innerHTML = router[ruta]
 }
